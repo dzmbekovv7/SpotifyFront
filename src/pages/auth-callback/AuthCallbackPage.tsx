d9b809722deaaf7fb587c2fd5bc0830a2efbfce1
Wizqqr <132731212+Wizqqr@users.noreply.github.com>
2025-06-14 17:05:46 +0600

    server

--- src/pages/auth-callback/AuthCallbackPage.tsx
+++ src/pages/auth-callback/AuthCallbackPage.tsx
@@ -1,6 +1,6 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { axiosInstance } from "@/lib/axios";
-import { useUser, AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import { useUser } from "@clerk/clerk-react";
 import { Loader } from "lucide-react";
 import { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
@@ -34,19 +34,15 @@ const AuthCallbackPage = () => {
 	}, [isLoaded, user, navigate]);
 
 	return (
-		<>
-			<AuthenticateWithRedirectCallback />
-			<div className='h-screen w-full bg-black flex items-center justify-center'>
-				<Card className='w-[90%] max-w-md bg-zinc-900 border-zinc-800'>
-					<CardContent className='flex flex-col items-center gap-4 pt-6'>
-						<Loader className='size-6 text-emerald-500 animate-spin' />
-						<h3 className='text-zinc-400 text-xl font-bold'>Logging you in</h3>
-						<p className='text-zinc-400 text-sm'>Redirecting...</p>
-					</CardContent>
-				</Card>
-			</div>
-		</>
+		<div className='h-screen w-full bg-black flex items-center justify-center'>
+			<Card className='w-[90%] max-w-md bg-zinc-900 border-zinc-800'>
+				<CardContent className='flex flex-col items-center gap-4 pt-6'>
+					<Loader className='size-6 text-emerald-500 animate-spin' />
+					<h3 className='text-zinc-400 text-xl font-bold'>Logging you in</h3>
+					<p className='text-zinc-400 text-sm'>Redirecting...</p>
+				</CardContent>
+			</Card>
+		</div>
 	);
 };
-
 export default AuthCallbackPage;
